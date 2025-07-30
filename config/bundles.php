9dd8314fe7c3e76ff783c152ea8e4aa032660dd0
ogougerot <olivier.gougerot@outlook.fr>
2025-07-30 17:20:10 +0200

    articles_done

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
